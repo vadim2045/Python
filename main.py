79b9f51a34fdf9fcdf1263cc9815c48bca320763
vadim2045 <vadimdah2@gmail.com>
2023-03-10 16:07:27 +0300

    Bug fix

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ print('Multiparadigmal programming languages : Lab 1')
 print('Pluzhnikov Vadim, IKM-221k')
 
 # Вывод имени 45 раз
-name = ["Vladyslav" for _ in range(45)]
-print(",".join(name))
+name = ['Vladyslav' for _ in range(45)]
+print(','.join(name))
 
 # Вычисление мат. формулы
 print((((123 - 23.1) * (2 ** 3 + 2.2)) / (127.24 - 21.1)) - 3.2)
